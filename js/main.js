7f79f93a93d425f3e527bef4f7eeda752e2db2ba
Moi.warez <Moi.warez@gmail.com>
2014-01-21 04:26:29 +0000

    Déplacement des fichiers pour tester la package. Mise en place de scripts JS pour gérer la leap et apprendre la gestion de classe et l'interaction avec node-webkit

--- js/main.js
+++ js/main.js
@@ -1,40 +1,40 @@
-var file = require('file.js');
 var gui = require('nw.gui');
-
-function clickInput(id) {
-    var event = document.createEvent('MouseEvents');
-    event.initMouseEvent('click');
-    document.getElementById(id).dispatchEvent(event);
-}
-
-document.addEventListener('keyup', function (e) {
-    if (e.keyCode == 'O'.charCodeAt(0) && e.ctrlKey) {
-        clickInput('open');
-    } else if (e.keyCode == 'S'.charCodeAt(0) && e.ctrlKey) {
-        clickInput('save');
-    } else if (e.keyCode == 'N'.charCodeAt(0) && e.ctrlKey) {
-        gui.Window.open('index.html');
-    }
-
-});
-
-document.getElementById('open').addEventListener('change', function (e) {
-    file.open(this.value, document);
-});
-
-document.getElementById('save').addEventListener('change', function (e) {
-    file.save(this.value, document);
-});
+//function clickInput(id) {
+//    var event = document.createEvent('MouseEvents');
+//    event.initMouseEvent('click');
+//    document.getElementById(id).dispatchEvent(event);
+//}
+//
+//document.addEventListener('keyup', function (e) {
+//    if (e.keyCode == 'O'.charCodeAt(0) && e.ctrlKey) {
+//        clickInput('open');
+//    } else if (e.keyCode == 'S'.charCodeAt(0) && e.ctrlKey) {
+//        clickInput('save');
+//    } else if (e.keyCode == 'N'.charCodeAt(0) && e.ctrlKey) {
+//        gui.Window.open('index.html');
+//    }
+//
+//});
+//
+//document.getElementById('open').addEventListener('change', function (e) {
+//    file.open(this.value, document);
+//});
+//
+//document.getElementById('save').addEventListener('change', function (e) {
+//    file.save(this.value, document);
+//});
 
 var menu = new gui.Menu({ type: 'menubar' });
 menu.append(new gui.MenuItem({
-    label: 'File',
+    label: 'Game',
     submenu: new gui.Menu()
 }));
 menu.items[0].submenu.append(new gui.MenuItem({
     label: 'New',
     click: function () {
-        gui.Window.open('index.html');
+//        $(window).load('domvisual.html');
+        var dv = require('domVisualizer.js');
+        dv.run(document, Leap);
     }
 }));
 menu.items[0].submenu.append(new gui.MenuItem({
